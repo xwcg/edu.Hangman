83e64f9fe0e8a5d6d1ffa629f2806ce9cf3a46e8
Michael Schwarz <x-w-a-r-e@gmx.net>
2014-05-19 14:56:44 +0200

    Projekt Anlegen

---
+++ main.c
@@ -0,0 +1,20 @@
+// Includes
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+/* * * * * * * * * * * * * * * * * * * * * *
+ * Klasse:  IFA 31
+ * Name:    Julian Roerig & Michael Schwarz
+ * Datum:   2014-05-19
+ * Beschreibung:
+ * Hangman... blabla
+ *
+ *
+ * * * * * * * * * * * * * * * * * * * * * */
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
