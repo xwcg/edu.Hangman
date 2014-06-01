62285d2338c072f1d25a4491276ca6d744d398a1
jroerig <julian.roerig@web.de>
2014-06-01 15:56:10 +0200

    Eingabe in For-Schleife mit Kleinbuchstaben
    0. Gestern geschriebenen Code komplett rausgeschmissen
    1. Quellcode ordentlicher hingeschrieben, nun ein mehrere Schleifen
    gelagert
    2. Umwandlung der Eingabe in Kleinbuchstaben (auch das Wort)
    3. Es wird nun angezeigt, welche Worte schon eingegeben wurden
    4. Grafische Ausgabe amateurhaft eingebaut

--- main.c
+++ main.c
@@ -10,10 +10,17 @@
  * Datum:   2014-05-31
  * Beschreibung:
  * Hangman... blabla
+ *
+ * Muss ergänzt werden:
+ * °beim 1. Versuch ein Wort eingeben erzeugt bei $geraten ein Komma (am besten anstatt i!=1, geraten ist noch leer nehmen
+ * °Ausgabe des Wortes mit Anzeige der bisher geraten Buchstaben
+ * °Bei richtig erratenem Wort goto einbinden
+ *
  * Quellenangaben: http://ascii.co.uk/art/hangman (Hangman Zeichnung)
  *                 http://www.peace-software.de/ckurs8.html (Arrays an Funktionen übergeben)
  *                 http://www.tutorials.at/c/09-arrays-strings.html (fgets (eingabe, 255, stdin);)
  *                 http://www.c-howto.de/ (Wissenswertes über Stringfunktionen)
+ *                 http://www.roseindia.net/c-tutorials/c-string-lowercase.shtml (String in Kleinbuchstaben umschreiben)
  * * * * * * * * * * * * * * * * * * * * * */
 
 int Grafik (int i, int leicht, char name[])
@@ -385,7 +392,6 @@ int Grafik (int i, int leicht, char name[])
     }
     return 0;
 }
-
 int GrafikTesten()
 {
     //durchläuft die Grafischen Bilder des Hangman, damit man prüfen kann, ob die Darstellung gut ist
@@ -411,7 +417,6 @@ int GrafikTesten()
 
     return 0;
 }
-
 int FunktionenKennenlernen()
 {
     //dient lediglich zu Übungszwecken und hat nichts mit dem eigentlichen Programm zu tun
@@ -434,95 +439,124 @@ int FunktionenKennenlernen()
 }
 int main()
 {
-    //dient zum kennenlernen der verschiedenen Funktionen
-    //FunktionenKennenlernen(); /*
-    int wied =0, i=0, laenge=0, treffer=0, fehler=0;
-
-
-    //simuliert die Grafikausgabe des Hangman
-    //GrafikTest(); /*
-
+    int wied=0;
     do
     {
-        char wort[100], geraten[100],eingabe[100];
-        printf("Bitte geben sie das zu erratende Wort ein.\n");
-        scanf("%s",&wort);
-        printf("Ihr Wort lautet = %s\n",wort);
-        printf("1. Eingabe:\n");
-
-        do
+        int i=0, i2=0, treffer=0, fehler=0;
+        char wort[100]="Wort", geraten[100]="", geratenWort[500]="", eingabe[100], name[30]="Julian Rörig";
+        //wandlet das Wort in Kleinbuchstaben um
+        for (i2 = 0; wort[i2]; i2++)
         {
-            scanf("%s",&eingabe);
-
-//prüft, ob der erste Buchstabe auch wirklich ein Buchstabe ist
-            if (isalpha(eingabe[0]))
-            {
-                printf("$eingabe ist ein Buchstabe\n");
-            }
-            else
-            {
-                printf("Bei der Eingabe handelt es sich um keinen Buchstaben.\n");
-                printf("Probieren Sie es noch einmal\n");
-            }
+            wort[i2] = tolower(wort[i2]);
         }
-        while(isalpha(eingabe[0])==0);
 
-                laenge = strlen(eingabe);
-                printf("Laenge der Eingabe: %i\n",laenge);
-                if (laenge == 1)
+
+        for(i=1; i<11; i++)
+
+        {
+            printf("Bisher geratene Buchstaben: %s\n",geraten);
+            printf("Bisher geratene Woerter: %s\n",geratenWort);
+            printf("%i Versuch\n",i);
+
+//wiederhole solange, bis ein Buchstabe angegeben wird, der noch nicht geraten wurde, und ein Buchstabe ist
+            do
+            {
+
+                scanf("%s",eingabe);
+                //wandelt die Eingabe in Kleinbuchstaben um
+                for (i2 = 0; eingabe[i2]; i2++)
                 {
-                    //hänge die eingabe hinten an die geratenen Einträge
-
-                    strcat(geraten, eingabe);
+                    eingabe[i2] = tolower(eingabe[i2]);
                 }
 
-
-                printf("2. Eingabe:\n");
-
-                //ließt so lange die eingabe ein, bis ein Buchstabe genannt ist, der noch nicht  in $geraten zu finden ist
-                do
+//es wurde nur ein Buchstabe eingegeben
+                if(strlen(eingabe)==1)
                 {
-                    scanf("%s",&eingabe);
+//Buchstabe wurde schon geraten
                     if(strstr(geraten, eingabe))
                     {
                         printf("Der gesuchte Buchstabe wurde schon geraten\n");
                         printf("Geben sie einen Buchstaben an, den sie noch nicht geraten haben\n");
+
+//Erstes Zeichen ist kein Buchstabe
                     }
-                }
-                while(strstr(geraten, eingabe));
+                    else if(isalpha(eingabe[0])==0)
+                    {
+                        printf("Bei der Eingabe handelt es sich um keinen Buchstaben.\n");
+                        printf("Probieren Sie es noch einmal\n");
+                    }
 
-                if (strstr(wort,eingabe))
-                {
-                    treffer=+1;
-                    printf("Wort enthaelt Buchstaben\n");
 
+
+
+//es wurde mehr als ein Buchstabe eingegeben
                 }
                 else
                 {
-
-                    fehler=+1;
-                    printf("%i. Fehler\n",fehler);
-                }
-                laenge = strlen(eingabe);
-                printf("Laenge der Eingabe: %i\n",laenge);
-                if (laenge == 1)
+                if (eingabe == wort)
                 {
-                    //hänge die eingabe hinten an die geratenen Einträge
-                    strcat(geraten,", ");
-                    strcat(geraten, eingabe);
+                    printf("Sie haben das Wort erraten\n");
+                    //goto
+                }
+                else
+                    {
+                        printf("Dies war nicht das zu erratene Wort\n");
+                    }
+
+
                 }
 
-                printf("Sie haben bisher folgende Buchstaben geraten: %s",geraten);
+            }
+            while(isalpha(eingabe[0])==0 || strstr(geraten, eingabe));
+
+//Buchstabe ist im Wort enthalten
+            if (strstr(wort,eingabe))
+            {
+                treffer= treffer + 1;
+                printf("Bisher %i Treffer bei %i Versuchen\n",treffer, i);
+
+//Buchstabe ist nicht im Wort enthalten
+            }
+            else
+            {
+                fehler= fehler + 1;
+                printf("Bisher %i Fehler bei %i Versuchen\n",fehler, i);
+            }
+
+//schreibt die eingaben hintereinander kommagetrennt in $geraten
+            //wenn es sich um den ersten Versuch handelt, lasse das Komma weg
 
 
+            if (strlen(eingabe)==1)
+            {
+                if (i!=1)
+                {
+                    strcat(geraten,", ");
+                }
+                strcat(geraten, eingabe);
 
+            }
+            else
+            {
+                if (i!=1)
+                {
+                    strcat(geratenWort,", ");
+                }
+                strcat(geratenWort, eingabe);
+            }
+
+            Grafik(fehler,1,name);
+            system("pause");
+            system("cls");
+        }
 
 
 
         printf("\n__________________________________________________________________________\n[1] = Wiederholen?\n");
         scanf("%i",&wied);
+        //setzt die eingaben wieder auf den Standard zurück
+
     }
     while(wied==1);
     return 0;
-
 }
