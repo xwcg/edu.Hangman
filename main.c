f6f8e87abac620a0370bff104451e8d49a9d51dd
jroerig <julian.roerig@web.de>
2014-06-05 23:06:11 +0200

    Ausgabe ausgelagert, Kommentare, Anforderungen

--- main.c
+++ main.c
@@ -13,10 +13,16 @@
  *
  * Muss ergänzt werden:
  * °Wort zufällig aus einer txt lesen
- * °Ausgabe des Wortes ohne Herzen
+ * °Ausgabe des Wortes ohne Herzen oder andere Sonderzeichen
  * °Bei richtig erratenem Wort goto einbinden (siehe Zeile 493)
  * °Statistik mit Name, Wort, Gewonnen/Verloren, Versuchen, Zeit in extra Datei schreiben und sortiert als Gesamtstatistik ausgeben
  * °Eingabe auf Länge prüfen, da auch "wo" / "to" ... als Treffer gezählt werden
+ * °wenn ein wort eingeben wurde, auch auf Sonderzeichen prüfen
+ * °Eine Hilfe mit einbauen
+ * °Ä,Ü,Ö,ß in ae, ue, oe, ss umwandeln und eine meldung ausgeben
+ * °ACHTUNG: AE, UE,OE können werden bisher nicht als einzelne Buchstaben gewertet.
+ * °Bei der while-Schleife muss geratenWort überprüft werden, da sonst nicht überprüft wird, ob ein Wort schon doppelt geraten wurde
+ * °Bei der while-Überprüfung eine Variable benutzen, welche aus ", " und $Eingabe besteht -> ist genauer
  *
  * Quellenangaben: http://ascii.co.uk/art/hangman (Hangman Zeichnung)
  *                 http://www.peace-software.de/ckurs8.html (Arrays an Funktionen übergeben)
@@ -419,32 +425,99 @@ int GrafikTesten()
 
     return 0;
 }
-int FunktionenKennenlernen()
+int Ausgabe(int i, int Reihenfolge, int error, char wort[100], char wortausgabe[100], char geraten[100], char geratenWort[500], char eingabe[100], char name [30], int treffer, int fehler, int gewonnen)
 {
-    //dient lediglich zu Übungszwecken und hat nichts mit dem eigentlichen Programm zu tun
-    do
-    {
-        char string[10];
-        char needle[10];
-        scanf("%s",&string);
-        scanf("%s",&needle);
 
-        if(strstr(string, needle))
+            system("cls");
+
+    Grafik(fehler,0,name);
+
+    if (i<=9)
+    {
+        printf("Das Wort lautet: %s\n",wortausgabe);
+    }
+    else
+    {
+        printf("Das Wort lautet: %s\n",wort);
+    }
+    printf("Bisher geratene Buchstaben: %s\n",geraten);
+    printf("Bisher geratene Woerter: %s\n",geratenWort);
+    if (i!=0)
+    {
+    printf("%i Treffer | %i Fehler | %i Versuch von 10 Versuchen\n",treffer,fehler, i);
+    }
+    if (i<=9)
+    {printf("%i Versuch\n",i+1);
+    }
+
+    if (Reihenfolge==2)
+    {
+        if (strlen(eingabe)==1)
         {
-            printf("String enthaelt %s\n",needle);;
+           printf("Der eingegebene Buchstabe wurde schon geraten\n");
+           printf("Geben Sie einen Buchstaben an, den Sie noch nicht geraten haben\n");
+        }
+        else
+        {
+           printf("Das eingegebene Wort wurde schon geraten\n");
+           printf("Geben Sie ein Wort an, den Sie noch nicht geraten haben\n");
         }
 
+        system("pause");
+        system("cls");
+    }
+
+    if (Reihenfolge==3)
+    {
+        if (strlen(eingabe)==1)
+        {
+        printf("Kein zulaessiger Buchstabe.\n");
+        printf("Probieren Sie es noch einmal\n");
+        }
+        else
+        {
+        printf("Kein zulaessiges Wort.\n");
+        printf("Probieren Sie es noch einmal\n");
+        }
+
+        system("pause");
+        system("cls");
+    }
+
+    if (Reihenfolge==4)
+    {
 
     }
-    while(1);
-    return 0;
+
+    if (Reihenfolge==5)
+    {
+
+    }
+
+    if (Reihenfolge==6)
+    {
+printf("\n__________________________________________________________________________\n[1] = Wiederholen?\n");
+    }
+
+    if (Reihenfolge==7)
+    {
+        printf("Dies ist das gesuchte Wort\n");
+        system("pause");
+    }
+
+    if (gewonnen ==1)
+    {
+        printf("Sie haben dieses Spiel erfolgreich abgeschlossen\n");
+        i = 11;
+    }
+
 }
 int main()
 {
     int wied=0;
     do
     {
-        int i=0, i2=0, i3=0, treffer=0, fehler=0, gewonnen=0;
+        int i=0, i2=0, i3=0, treffer=0, fehler=0, gewonnen=0, gewonnenWort, error;
         char wort[100]="Wort", wortausgabe[100]="", geraten[100]="", geratenWort[500]="", eingabe[100], name[30]="Julian Rörig";
         //wandlet das Wort in Kleinbuchstaben um
         for (i2 = 0; wort[i2]; i2++)
@@ -452,18 +525,17 @@ int main()
             wort[i2] = tolower(wort[i2]);
         }
 
-
-        for(i=1; i<11; i++)
+//10 Versuche bis Game Over
+        for(i=0; i<10; i++)
 
         {
-            printf("Bisher geratene Buchstaben: %s\n",geraten);
-            printf("Bisher geratene Woerter: %s\n",geratenWort);
-            printf("%i Versuch\n",i);
+
+
 
 //wiederhole solange, bis ein Buchstabe angegeben wird, der noch nicht geraten wurde, und ein Buchstabe ist
             do
             {
-
+Ausgabe(i,1,error,wort,wortausgabe,geraten,geratenWort,eingabe,name,treffer,fehler,gewonnen);
                 scanf("%s",eingabe);
                 //wandelt die Eingabe in Kleinbuchstaben um
                 for (i2 = 0; eingabe[i2]; i2++)
@@ -471,63 +543,64 @@ int main()
                     eingabe[i2] = tolower(eingabe[i2]);
                 }
 
+
 //es wurde nur ein Buchstabe eingegeben
                 if(strlen(eingabe)==1)
                 {
 //Buchstabe wurde schon geraten
                     if(strstr(geraten, eingabe))
                     {
-                        printf("Der gesuchte Buchstabe wurde schon geraten\n");
-                        printf("Geben sie einen Buchstaben an, den sie noch nicht geraten haben\n");
+                        error =1;
+                        Ausgabe(i,2,error,wort,wortausgabe,geraten,geratenWort,eingabe,name,treffer,fehler,gewonnen);
+
 
 //Erstes Zeichen ist kein Buchstabe
                     }
                     else if(isalpha(eingabe[0])==0)
                     {
-                        printf("Bei der Eingabe handelt es sich um keinen Buchstaben.\n");
-                        printf("Probieren Sie es noch einmal\n");
+                        error =1;
+                        Ausgabe(i,3,error,wort,wortausgabe,geraten,geratenWort,eingabe,name,treffer,fehler,gewonnen);
+
+                    }
+                    else
+                    {
+                        error = 0;
                     }
 
 
 
 
-//es wurde mehr als ein Buchstabe eingegeben
-//wird nicht benötigt, da unten das ganze Wort überprüft wird und so die Ausgabe erfolgt (siehe:   if (wortausgabe[i3] == wort [i3])  )          }
-                    /*    else
-                         {
-
-                             if (eingabe == wort)
-                             {
-                                 printf("Sie haben das Wort erraten\n");
-                                 //goto
-                             }
-                             else
-                             {
-                                 printf("Dies war nicht das zu erratene Wort\n");
-                             }
-
-
-                         }
-                    */
                 }
+
             }
             while(isalpha(eingabe[0])==0 || strstr(geraten, eingabe));
 
 //Buchstabe ist im Wort enthalten
-            if (strstr(wort,eingabe))
+//Zählt die richtigen Treffer
+            if (strlen(eingabe)==1 && strstr(wort,eingabe))
             {
                 treffer= treffer + 1;
-                printf("Bisher %i Treffer bei %i Versuchen\n",treffer, i);
+                Ausgabe(i,0,error,wort,wortausgabe,geraten,geratenWort,eingabe,name,treffer,fehler,gewonnen);
 
-//Buchstabe ist nicht im Wort enthalten
             }
+            else if (strlen(eingabe)==strlen(wort) && strcmp(wort,eingabe)==0)
+            {
+                treffer=treffer+1;
+                Ausgabe(i,7,error,wort,wortausgabe,geraten,geratenWort,eingabe,name,treffer,fehler,gewonnen);
+                gewonnen=1;
+            }
+//Buchstabe ist nicht im Wort enthalten
+//Zählt die Fehlversuche
+
             else
             {
                 fehler= fehler + 1;
-                printf("Bisher %i Fehler bei %i Versuchen\n",fehler, i);
+                Ausgabe(i,0,error,wort,wortausgabe,geraten,geratenWort,eingabe,name,treffer,fehler,gewonnen);
+
+
             }
 
-//schreibt die eingaben hintereinander kommagetrennt in $geraten
+//schreibt die Eingaben hintereinander kommagetrennt in $geraten
             //wenn es sich um den ersten Versuch handelt, lasse das Komma weg
 
 
@@ -542,6 +615,7 @@ int main()
             }
             else
             {
+                //schreibt die geratenen Worte kommagetrennt in $geratenWort
                 if (strlen(geratenWort) != 0)
                 {
                     strcat(geratenWort,", ");
@@ -549,10 +623,11 @@ int main()
                 strcat(geratenWort, eingabe);
             }
 
-//Gibt die zu lösenden Buchstaben aus
 
+//Gibt die zu lösenden Buchstaben aus
             if (strlen(eingabe)==1)
             {
+                //geht jeden Buchstaben in $wort durch und schreibt wenn die Eingabe einem Buchstaben entspricht, diesen in $wortausgabe, ansonsten "_"
                 for(i3=0; i3 < strlen(wort); i3++)
                 {
                     if (wort[i3] == eingabe[0])
@@ -568,28 +643,27 @@ int main()
                 }
             }
 
-            printf("Das Wort lautet: %s\n",wortausgabe);
+
 
             if (strcmp(wort, wortausgabe)==0)
             {
                 gewonnen=1;
             }
 
-
-            if (gewonnen == 1)
+            if (strcmp(wort, eingabe)==0)
             {
-                printf("Sie haben gewonnen!\n");
+                gewonnen=1;
             }
 
+            Ausgabe(i,0,error,wort,wortausgabe,geraten,geratenWort,eingabe,name,treffer,fehler,gewonnen);
 
-            Grafik(fehler,1,name);
-            system("pause");
-            system("cls");
+
+
         }
 
 
 
-        printf("\n__________________________________________________________________________\n[1] = Wiederholen?\n");
+         Ausgabe(i,6,error,wort,wortausgabe,geraten,geratenWort,eingabe,name,treffer,fehler,gewonnen);
         scanf("%i",&wied);
         //setzt die eingaben wieder auf den Standard zurück
 
